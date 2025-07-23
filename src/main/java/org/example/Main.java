b3565a3c18bdc258e90704a272a15da70deb120c
sena <gulozseven@gmail.com>
2025-07-23 16:23:03 +0300

    Data Structures Challenge 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,35 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
+
+  public static boolean checkForPalindrome(String string) {
+      String kelime = string.toLowerCase().replaceAll("[ .,?!_-]", "");
+      String[] arraykelime = kelime.split("");
+      List<String> arrayList = new ArrayList<>(List.of(arraykelime));
+      List<String> reversedArray = new ArrayList<>();
+
+      for (int i = arrayList.size() - 1; i >= 0; i-- ) {
+          reversedArray.add(arrayList.get(i));
+      }
+      return reversedArray.equals(arrayList);
+  }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(checkForPalindrome("I did, did I?"));
+        System.out.println(checkForPalindrome("Racecar"));
+        System.out.println(checkForPalindrome("hello"));
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+
+
+        
     }
 
 }
---
+++ src/main/java/org/example/WorkintechList.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkintechList extends ArrayList<String> {
+}
