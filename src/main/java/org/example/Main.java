027f65037cebbe5217e7232adea4cef3fc062b69
sena <gulozseven@gmail.com>
2025-07-23 17:12:47 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,17 +9,48 @@ import java.util.List;
 public class Main {
 
 
-  public static boolean checkForPalindrome(String string) {
-      String kelime = string.toLowerCase().replaceAll("[ .,?!_-]", "");
-      String[] arraykelime = kelime.split("");
-      List<String> arrayList = new ArrayList<>(List.of(arraykelime));
-      List<String> reversedArray = new ArrayList<>();
+    public static boolean checkForPalindrome(String string) {
+        String kelime = string.toLowerCase().replaceAll("[ .,?!_-]", "");
+        String[] arraykelime = kelime.split("");
+        List<String> arrayList = new ArrayList<>(List.of(arraykelime));
+        List<String> reversedArray = new ArrayList<>();
 
-      for (int i = arrayList.size() - 1; i >= 0; i-- ) {
-          reversedArray.add(arrayList.get(i));
-      }
-      return reversedArray.equals(arrayList);
-  }
+        for (int i = arrayList.size() - 1; i >= 0; i--) {
+            reversedArray.add(arrayList.get(i));
+        }
+        return reversedArray.equals(arrayList);
+    }
+
+
+    public static String convertDecimalToBinary(int i) {
+        String binaryNum = "";
+        int sayi = i;
+
+        while (true) {
+
+            if (sayi == 0) {
+                binaryNum += "0";
+                break;
+            } else if (sayi == 1) {
+                binaryNum += "1";
+                break;
+            }
+
+            if (sayi % 2 == 1) {
+                binaryNum += "1";
+                sayi = sayi / 2;
+            } else {
+                binaryNum += "0";
+                sayi = sayi / 2;
+            }
+        }
+
+        String reverse = "";
+        for (int o = binaryNum.length() - 1; o >= 0; o--) {
+            reverse += binaryNum.charAt(o);
+        }
+        return reverse;
+    }
 
 
     public static void main(String[] args) {
@@ -28,8 +59,12 @@ public class Main {
         System.out.println(checkForPalindrome("hello"));
         System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
 
-
-        
+        WorkintechList workintech = new WorkintechList();
+        workintech.add("ucubik");
+        workintech.add("ucubik");
+        System.out.println(workintech);
+        System.out.println(convertDecimalToBinary(13));
     }
+
 
 }
--- src/main/java/org/example/WorkintechList.java
+++ src/main/java/org/example/WorkintechList.java
@@ -1,7 +1,30 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
-public class WorkintechList extends ArrayList<String> {
+
+public class WorkintechList extends ArrayList {
+
+    @Override
+    public boolean add(Object o) {
+        if (this.contains(o)) {
+            System.out.println("Listede zaten var.");
+            return false;
+        } else {
+            super.add(o);
+            System.out.println("listeye eklendi.");
+            return true;
+        }
+    }
+
+    public void sort() {
+        Collections.sort(this);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        sort();
+        return super.remove(o);
+    }
 }
